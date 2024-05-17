f4e76bc29075a2f64b55ff04a8b000f510bbddfa
rahul nayak <rahul_nayak@thbs.com>
2024-05-17 16:30:09 +0530

    1111

--- src/QRScanner.jsx
+++ src/QRScanner.jsx
@@ -1,17 +1,6 @@
-import { useEffect } from 'react';
 import { Scanner } from '@yudiel/react-qr-scanner';
 
 const QRScanner = () => {
-    useEffect(() => {
-        navigator.mediaDevices.getUserMedia({ video: true })
-            .then(stream => {
-                // Do nothing, just request the camera access
-            })
-            .catch(err => {
-                console.error("Error accessing the camera: ", err);
-            });
-    }, []);
-
     return (
         <div style={{ maxWidth: '300px', maxHeight: '300px', margin: 'auto' }}>
             <Scanner
